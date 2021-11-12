d403cd6ac33413a348a3ac08dabffc5eb0a0a492
digi-coder <maurizio_de_luca@hotmail.com>
2021-11-12 11:24:23 -0800

    Edit index.html and eas.js

---
+++ js/eas.js
@@ -0,0 +1,23 @@
+const easapp = () => {
+    const gridSizeButtons = document.querySelectorAll('.size');
+    const penColorButtons = document.querySelectorAll('.mode');
+
+    let currentMode = '';
+
+    function generateGrid(size = 32 * 44, cssClass = 'medium-grid-default') {
+        const gameContainer = document.getElementById('grid-container');
+
+        gameContainer.innerHTML = '';
+
+        gameContainer.classList.remove('small-grid', 'medium-grid-default', 'large-grid');
+        gameContainer.classList.add(cssClass);
+
+        for (let i = 0; i < size; i += 1) {
+            const div = document.createElement ('div');
+            gameContainer.appendChild(div);
+        }
+    }
+}
+
+//Start the easapp function
+easapp();
