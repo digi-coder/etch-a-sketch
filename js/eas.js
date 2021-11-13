193ead3fc8e2126c84fea2ceeafd6102a8bafe06
digi-coder <maurizio_de_luca@hotmail.com>
2021-11-12 21:06:06 -0800

    Andy edited index.html and eas.js

--- js/eas.js
+++ js/eas.js
@@ -13,7 +13,7 @@ const easapp = () => {
         gameContainer.classList.add(cssClass);
 
         for (let i = 0; i < size; i += 1) {
-            const div = document.createElement ('div');
+            const div = document.createElement('div');
             gameContainer.appendChild(div);
         }
     }
@@ -21,50 +21,50 @@ const easapp = () => {
 
     function erase() {
         const gridItems = document.querySelectorAll('#grid-container > div');
-      
-        gridItems.forEach((item) => {
-          item.style.backgroundColor = '#D8D8D8';
-          item.style.opacity = '1';
-        });
-      }
 
-      
+        gridItems.forEach((item) => {
+            item.style.backgroundColor = '#D8D8D8';
+            item.style.opacity = '1';
+        });
+    }
+
+
     function startPainting(mode) {
         const gridItems = document.querySelectorAll('#grid-container > div');
-      
+
         gridItems.forEach((item) => {
-          item.count = 0;
-          item.addEventListener('mouseenter', (e) => {
-            if (mode === 'gray' || currentMode === 'gray' || currentMode === '') {
-              e.target.style.backgroundColor = '#707070';
-              e.target.style.opacity = 1;
-            } else if (mode === 'black' || currentMode === 'black') {
-              e.target.style.backgroundColor = '#000000';
-              e.target.count += 1;
-              e.target.style.opacity = 0.2 * e.target.count;
-            } else if (mode === 'rainbow' || currentMode === 'rainbow') {
-              const rainbowPallete = ['#EF476F', '#FFD166', '#06D6A0', '#118AB2', '#073B4C'];
-              const randomColor = Math.floor(Math.random() * rainbowPallete.length);
-              e.target.style.opacity = 1;
-              e.target.style.backgroundColor = rainbowPallete[randomColor];
-            }
-          });
+            item.count = 0;
+            item.addEventListener('mouseenter', (e) => {
+                if (mode === 'gray' || currentMode === 'gray' || currentMode === '') {
+                    e.target.style.backgroundColor = '#707070';
+                    e.target.style.opacity = 1;
+                } else if (mode === 'black' || currentMode === 'black') {
+                    e.target.style.backgroundColor = '#000000';
+                    e.target.count += 1;
+                    e.target.style.opacity = 0.2 * e.target.count;
+                } else if (mode === 'rainbow' || currentMode === 'rainbow') {
+                    const rainbowPallete = ['#EF476F', '#FFD166', '#06D6A0', '#118AB2', '#8BD175'];
+                    const randomColor = Math.floor(Math.random() * rainbowPallete.length);
+                    e.target.style.opacity = 1;
+                    e.target.style.backgroundColor = rainbowPallete[randomColor];
+                }
+            });
         });
-      }
+    }
 
 
-      function selectButton(button) {
+    function selectButton(button) {
         if (button.classList.contains('mode')) {
-          penColorButtons.forEach((selection) => {
-            selection.classList.remove('active-button');
-          });
+            penColorButtons.forEach((selection) => {
+                selection.classList.remove('active-button');
+            });
         } else {
-          gridSizeButtons.forEach((selection) => {
-            selection.classList.remove('active-button');
-          });
+            gridSizeButtons.forEach((selection) => {
+                selection.classList.remove('active-button');
+            });
         }
         button.classList.add('active-button');
-      }
+    }
 
 
     function changeSize() {
@@ -76,59 +76,59 @@ const easapp = () => {
 
         gridSizeButtons.forEach((selection) => {
             selection.addEventListener('click', () => {
-              if (selection.classList.contains('small')) {
-                //erase();
-                generateGrid(small, 'small-grid');
-                startPainting();
-                selectButton(selection);
-              } else if (selection.classList.contains('medium')) {
-                //erase();
-                generateGrid(medium, 'medium-grid-default');
-                startPainting();
-                selectButton(selection);
-              } else {
-                //erase();
-                generateGrid(large, 'large-grid');
-                startPainting();
-                selectButton(selection);
-              }
+                if (selection.classList.contains('small')) {
+                    //erase();
+                    generateGrid(small, 'small-grid');
+                    startPainting();
+                    selectButton(selection);
+                } else if (selection.classList.contains('medium')) {
+                    //erase();
+                    generateGrid(medium, 'medium-grid-default');
+                    startPainting();
+                    selectButton(selection);
+                } else {
+                    //erase();
+                    generateGrid(large, 'large-grid');
+                    startPainting();
+                    selectButton(selection);
+                }
             });
-          });
+        });
     }
 
 
     function changeColor() {
         penColorButtons[0].classList.add('active-button');
-      
+
         penColorButtons.forEach((selection) => {
-          selection.addEventListener('click', () => {
-            if (selection.classList.contains('gray')) {
-              startPainting('gray');
-              selectButton(selection);
-              currentMode = 'gray';
-            } else if (selection.classList.contains('black')) {
-              startPainting('black');
-              selectButton(selection);
-              currentMode = 'black';
-            } else {
-              startPainting('rainbow');
-              selectButton(selection);
-              currentMode = 'rainbow';
-            }
-          });
+            selection.addEventListener('click', () => {
+                if (selection.classList.contains('gray')) {
+                    startPainting('gray');
+                    selectButton(selection);
+                    currentMode = 'gray';
+                } else if (selection.classList.contains('black')) {
+                    startPainting('black');
+                    selectButton(selection);
+                    currentMode = 'black';
+                } else {
+                    startPainting('rainbow');
+                    selectButton(selection);
+                    currentMode = 'rainbow';
+                }
+            });
         });
-      }
+    }
 
 
     function eraseListener() {
         const eraseButton = document.querySelector('.erase');
-        
+
         eraseButton.addEventListener('click', erase);
     }
 
 
 
-    generateGrid(); 
+    generateGrid();
     startPainting();
     changeSize();
     changeColor();
